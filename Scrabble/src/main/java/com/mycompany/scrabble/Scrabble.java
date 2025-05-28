74676110f0c1c633c63fa304907422ac371ad07a
DanielBA05 <luisdanielba3@gmail.com>
2025-05-28 16:06:03 -0600

    registro

--- Scrabble/src/main/java/com/mycompany/scrabble/Scrabble.java
+++ Scrabble/src/main/java/com/mycompany/scrabble/Scrabble.java
@@ -5,8 +5,7 @@ import javax.swing.SwingUtilities;
 public class Scrabble {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Tablero tablero = new Tablero();
-            new TableroVistaSwing(tablero);
+            new PantallaRegistro();
         });
     }
 }
