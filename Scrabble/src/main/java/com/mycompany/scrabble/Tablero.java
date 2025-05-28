dbf8b0c6256e70f55a7244718bcdd2b5c03da9ef
DanielBA05 <luisdanielba3@gmail.com>
2025-05-28 13:21:26 -0600

    AjusteParesOrdenados

--- Scrabble/src/main/java/com/mycompany/scrabble/Tablero.java
+++ Scrabble/src/main/java/com/mycompany/scrabble/Tablero.java
@@ -20,48 +20,55 @@ public class Tablero {
     }
 
     private int obtenerBonificador(int x, int y) {
-    // Esquinas y puntos medios de los bordes: Triple Palabra (TRIPP)
-    if ((x == 0 || x == 14) && (y == 0 || y == 7 || y == 14)) {
-        return Casilla.TRIPP;
-    }
-    if ((y == 0 || y == 14) && (x == 7)) {
-        return Casilla.TRIPP;
+        // Triple Palabra (TRIPP)
+        int[][] triplePalabra = {
+            {0, 0}, {0, 7}, {0, 14},
+            {7, 0}, {7, 14},
+            {14, 0}, {14, 7}, {14, 14}
+        };
+
+        // Doble Palabra (DOUP)
+        int[][] doblePalabra = {
+            {1, 1}, {2, 2}, {3, 3}, {4, 4}, {10, 10}, {11, 11}, {12, 12}, {13, 13},
+            {1, 13}, {2, 12}, {3, 11}, {4, 10}, {10, 4}, {11, 3}, {12, 2}, {13, 1},
+            {7, 7} // centro
+        };
+
+        // Triple Letra (TRIPL)
+        int[][] tripleLetra = {
+            {5, 1}, {5, 5}, {5, 9}, {5, 13},
+            {9, 1}, {9, 5}, {9, 9}, {9, 13},
+            {1, 5}, {1, 9}, {13, 5}, {13, 9}
+        };
+
+        // Doble Letra (DOUL)
+        int[][] dobleLetra = {
+            {0, 3}, {0, 11}, {2, 6}, {2, 8},
+            {3, 0}, {3, 7}, {3, 14}, {6, 2}, {6, 6}, {6, 8}, {6, 12},
+            {7, 3}, {7, 11}, {8, 2}, {8, 6}, {8, 8}, {8, 12},
+            {11, 0}, {11, 7}, {11, 14}, {12, 6}, {12, 8}, {14, 3}, {14, 11}
+        };
+
+        for (int[] pos : triplePalabra) {
+            if (x == pos[0] && y == pos[1]) return Casilla.TRIPP;
+        }
+
+        for (int[] pos : doblePalabra) {
+            if (x == pos[0] && y == pos[1]) return Casilla.DOUP;
+        }
+
+        for (int[] pos : tripleLetra) {
+            if (x == pos[0] && y == pos[1]) return Casilla.TRIPL;
+        }
+
+        for (int[] pos : dobleLetra) {
+            if (x == pos[0] && y == pos[1]) return Casilla.DOUL;
+        }
+
+        // Casilla normal
+        return Casilla.NONE;
     }
 
-    // Centro: Doble Palabra (DOUP)
-    if (x == 7 && y == 7) {
-        return Casilla.DOUP;
-    }
-
-    // Diagonales (excepto esquinas y centro): Doble Palabra (DOUP)
-    if ((x == y || x + y == 14) && x != 0 && x != 7 && x != 14 && x!=6 && x!=8 && y!=6 && y!=8 && y != 0 && y != 7 && y != 14 ) {
-        return Casilla.DOUP;
-    }
-
-    // Triple Letra (TRIPL) 
-    if ((x == 5 && (y == 1 || y == 5 || y == 9 || y == 13)) ||
-        (x == 9 && (y == 1 || y == 5 || y == 9 || y == 13)) ||
-        ((y == 5 || y == 9) && (x == 1 || x == 5 || x == 9 || x == 13))) {
-        return Casilla.TRIPL;
-    }
-
-    // Doble Letra (DOUL) 
-    if ((x == 3 || x == 11) && (y == 0 || y == 7 || y == 14)) {
-        return Casilla.DOUL;
-    }
-    if ((y == 3 || y == 11) && (x == 0 || x == 7 || x == 14)) {
-        return Casilla.DOUL;
-    }
-    if ((x == 6 || x == 8) && (y == 2 || y == 6 || y == 8 || y == 12)) {
-        return Casilla.DOUL;
-    }
-    if ((y == 6 || y == 8) && (x == 2 || x == 6 || x == 8 || x == 12)) {
-        return Casilla.DOUL;
-    }
-
-    // Resto: casillas normales
-    return Casilla.NONE;
-}
 
 
     // Métodos clave
